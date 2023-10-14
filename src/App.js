6a0883584c82034057f52ff1b70bcc7ce201af63
blloop <42186044+blloop@users.noreply.github.com>
2023-10-14 14:59:29 -0700

    add basic page structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import page1 from './components/page1';
+import page2 from './components/page2';
+import page3 from './components/page3';
+import page4 from './components/page4';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let pageIndex = 1;
+
+const App = () => {
+  switch (pageIndex) {
+    case 1: 
+      return page1();
+    case 2: 
+      return page2();
+    case 3: 
+      return page3();
+    case 4: 
+      return page4();
+  }
 }
 
 export default App;
